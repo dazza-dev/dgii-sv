70f3364a310d845f19461d3ab0901e621f48f464
Dazza <adaza90@outlook.com>
2025-10-25 13:48:25 -0500

    Update ThirdPartySale.php

--- src/Models/Body/ThirdPartySale.php
+++ src/Models/Body/ThirdPartySale.php
@@ -4,48 +4,77 @@ namespace DazzaDev\DgiiSv\Models\Body;
 
 class ThirdPartySale
 {
+    /**
+     * NIT of the third party
+     */
     private string $nit = '';
 
+    /**
+     * Name of the third party
+     */
     private string $name = '';
 
+    /**
+     * Third party sale constructor
+     */
     public function __construct(array $data = [])
     {
         $this->initialize($data);
     }
 
+    /**
+     * Initialize third party sale data
+     */
     private function initialize(array $data): void
     {
+        if (empty($data)) {
+            return;
+        }
+
         if (isset($data['nit'])) {
             $this->setNit($data['nit']);
         }
 
-        if (isset($data['nombre'])) {
-            $this->setName($data['nombre']);
-        } elseif (isset($data['name'])) {
+        if (isset($data['name'])) {
             $this->setName($data['name']);
         }
     }
 
+    /**
+     * Get third party sale NIT
+     */
     public function getNit(): string
     {
         return $this->nit;
     }
 
+    /**
+     * Set third party sale NIT
+     */
     public function setNit(string $nit): void
     {
         $this->nit = $nit;
     }
 
+    /**
+     * Get third party sale name
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Set third party sale name
+     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
+    /**
+     * Convert third party sale to array
+     */
     public function toArray(): array
     {
         return [
