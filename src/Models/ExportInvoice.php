56d2faf022bec1241a2856f1efce65fc840b0d6f
Dazza <adaza90@outlook.com>
2025-10-30 11:14:26 -0500

    Create ExportInvoice.php

---
+++ src/Models/ExportInvoice.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DazzaDev\DgiiSv\Models;
+
+use DazzaDev\DgiiSv\Models\Base\Document;
+use DazzaDev\DgiiSv\Traits\JsonTrait;
+
+class ExportInvoice extends Document
+{
+    use JsonTrait;
+
+    /**
+     * ExportInvoice constructor
+     */
+    public function __construct(array $data = [])
+    {
+        $this->setDocumentType('11');
+        $this->setVersion(1);
+
+        // Initialize export invoice data
+        parent::__construct($data);
+    }
+
+    /**
+     * Get array representation
+     */
+    public function toArray(): array
+    {
+        return parent::toArray();
+    }
+}
